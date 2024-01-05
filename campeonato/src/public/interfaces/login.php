7ec228188cc38a9ed293cf3e01a40707a6b84e9c
NIcknameInvalido <nikooza1@gmail.com>
2024-01-05 17:40:32 -0300

    Tela de login adicionada

---
+++ campeonato/src/public/interfaces/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+
+<body>
+    <div class="page">
+        <div class="card">
+            <form>
+                <h2>Login</h2>
+                <label for="user">Usuário</label>
+                <input type="user" class="campo">
+                <label for="password">Senha</label>
+                <input type="password" class="campo">
+                <input type="submit" value="Acessar">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
